0d612e4818ee7149c95c27157ea3a8f996db3306
faosldkjfhf <60448325+faosldkjfhf@users.noreply.github.com>
2023-04-23 13:45:32 -0400

    dao fixed

--- controllers/lists/lists-dao.js
+++ controllers/lists/lists-dao.js
@@ -4,4 +4,4 @@ export const findListById = (lid) => listsModel.findById(lid);
 export const findListsByUser = (uid) => listsModel.find({ userId: uid });
 export const createList = (list) => listsModel.create(list);
 export const updateList = (list) => listsModel.updateOne({ _id: list._id }, { $set: list });
-export const deletePost = (lid) => listsModel.deleteOne({ _id: lid });
+export const deleteList = (lid) => listsModel.deleteOne({ _id: lid });
